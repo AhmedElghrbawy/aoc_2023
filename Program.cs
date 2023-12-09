02fba3a063ead3682616eccb61452611b9aba210
Ahmed Elghrbawy <ahmed.elghrbawy16@gmail.com>
2023-12-09 11:52:53 +0200

    day_9

--- Program.cs
+++ Program.cs
@@ -4,71 +4,39 @@ string path = @"input.txt";
 
 
 var lines = File.ReadAllLines(path);
-
-var directionIndex = new Dictionary<char, int>()
-{
-    {'L', 0},
-    {'R', 1}
-};
-
-var adjList = new Dictionary<string, string[]>();
-var directions = lines[0];
-
-for (int i = 2; i < lines.Length; i++)
-{
-    var line = lines[i];
-    var l = line.Split("=");
-    string node = l[0].Trim();
-
-    var edges = l[1].Trim().Replace("(", "").Replace(")", "").Split(',', StringSplitOptions.TrimEntries);
-    adjList[node] = edges;
-}
-
-// System.Console.WriteLine(adjList.Count); 802
-
 long ans = 0;
-var inProgress = adjList.Keys.Where(k => k.EndsWith('A')).ToArray();
-System.Console.WriteLine(inProgress.Length);
-var dp = new Dictionary<string, HashSet<int>>();
 
-
-foreach (var item in inProgress)
+foreach (var line in lines)
 {
-    string node = item;
-    var zSteps = new HashSet<long>();
-    long step = 0;
+    var nums = line.Split().Select(num => Int64.Parse(num)).ToList();
+    var first = new Dictionary<long, long>();
 
+    var levels = new List<List<long>>();
+    levels.Add(nums);
+    first[0] = nums[0];
+    long curlevel = 1;
 
-    for (int i = 0; i < directions.Length; i = (i + 1) % directions.Length)
+    while (levels[^1].Any(num => num != 0))
     {
-        if (node.EndsWith('Z'))
-            zSteps.Add(step);
+        var lastLevel = levels[^1];
 
-        if (zSteps.Count == inProgress.Length)
+        var newLevel = new List<long>();
+        for (int i = 1; i < lastLevel.Count; i++)
         {
-            System.Console.WriteLine("Found all");
-            break;
-        }
-        if (dp.ContainsKey(node) && dp[node].Contains(i))
-        {
-            System.Console.WriteLine("That is a cycle bro");
-            System.Console.WriteLine((node, i));
-            break;
+            newLevel.Add(lastLevel[i] - lastLevel[i - 1]);
         }
 
-        if (!dp.ContainsKey(node))
-            dp[node] = new();
-        
-        dp[node].Add(i);
-
-
-        char direction = directions[i];
-        node = adjList[node][directionIndex[direction]];
-        step++;
+        levels.Add(newLevel);
+        first[curlevel] = newLevel[0];
+        curlevel++;
     }
 
-    System.Console.WriteLine(String.Join(" ", zSteps));
+    for (int i = levels.Count - 2; i >= 0; i--)
+    {
+        first[i] = first[i] - first[i + 1];
+    }
+
+    ans += first[0];
 }
 
-// this problem is not well descriped
 System.Console.WriteLine(ans);
